027f9e4621ff2ee5ddcc86bc43f5dae25780c901
Shaul Fridman <154060414+shaulnv@users.noreply.github.com>
2024-11-10 13:39:16 +0200

    (+) Cpp Starter Kit

---
+++ include/starterkit/starterkit.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+void starterkit_lib();
+void starterkit_lib_print_vector(const std::vector<std::string> &strings);
+int starterkit_lib_answer();
