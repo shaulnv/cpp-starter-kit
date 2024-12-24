af7d05f48641ab0a3137b56363c7169cc00517b2
Shaul Fridman <shaulf@nvidia.com>
2024-12-24 17:13:48 +0200

    (+) add support for building to web assembly
    1. Add `--profile` to `build.sh`
       You can build to `native`(linux) or `wasm`.
       `wasm` module is run from nodejs.
    2. Add `--clean` to `build.sh`
       Delete the build folder before building
    3. A lot of other refactorings that I wish
       I would have done more proffecianly in a separte commits :-|

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-source ./env/env_vars.sh
-exec "$build_folder/cli/starterkit" "$@"
+# activate python's virtual env
+. ./activate.sh --quiet
+. .env 2>&1 >/dev/null || true
+
+# make sure we built the project
+if [ ! -f "$cli_path" ]; then
+  echo "Error: Executable not found at $cli_path"
+  echo "Please build the project first by running: ./build.sh"
+  exit 1
+fi
+
+exec $driver "$cli_path" "$@"
