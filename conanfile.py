c60141996a2993c924992b13f881d5f1589875e9
Shaul Fridman <shaulf@nvidia.com>
2024-12-24 17:13:48 +0200

    (+) add support for building to web assembly
    1. Add `--profile` to `build.sh`
       You can build to `native`(linux) or `wasm`.
       `wasm` module is run from nodejs.
    2. Add `--clean` to `build.sh`
       Delete the build folder before building
    3. A lot of other refactorings that I wish
       I would have done more proffecianly in a separte commits :-|

--- conanfile.py
+++ conanfile.py
@@ -39,7 +39,7 @@ class starterkit_libRecipe(ConanFile):
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
-        tc = CMakeToolchain(self, generator="Ninja")
+        tc = CMakeToolchain(self)
         tc.generate()
 
     def build(self):
