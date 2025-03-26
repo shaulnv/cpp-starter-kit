166274afbf2a6c65abe341da059c7cdb421d8865
Shaul Fridman <shaulf@nvidia.com>
2025-03-26 16:38:17 +0200

    (~) refactor bash scripts for brevity
    Not proud of this commit, as it is a lot of changes.
    Mostly it is a refctoring and fixing the build scripts.

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# activate python's virtual env
-. ./activate.sh --quiet
+# take the install prefix from the command line / env vars file
+install_prefix=$1
+source env/_env_vars_file.sh
 
 # Check if prefix parameter is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <install-prefix>"
-    echo "Example: $0 /usr/local"
-    exit 1
+if [ $# -ne 1 ] && [ "$install_prefix" = "" ]; then
+  echo "Usage: $0 <install-prefix>"
+  echo "Example: $0 /usr/local"
+  exit 1
 fi
+update_env_vars_file $root_dir/.env install_prefix $install_prefix
 
 # build the project
 ./build.sh
-. .env >/dev/null 2>&1 || true
+# update the env vars if changed
+source _env_vars_file.sh
 
-# Perform the installation
-cmake --install "$build_folder" --prefix "$1" 
+# activate python's virtual env
+source ./activate.sh --quiet
+cmake --install "$build_folder" --prefix "$install_prefix"
