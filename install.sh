12fd65d7151f442eef8aebc85118df2432c3c950
Shaul Fridman <shaulf@nvidia.com>
2025-04-05 03:30:27 +0300

    (~) move 'cmake' folder to env, move private scripts to their own folder

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # take the install prefix from the command line / env vars file
 install_prefix=$1
-source env/_env_vars_file.sh
+source env/scripts/private/_env_vars_file.sh
 
 # Check if prefix parameter is provided
 if [ $# -ne 1 ] && [ "$install_prefix" = "" ]; then
@@ -15,7 +15,7 @@ update_env_vars_file $root_dir/.env install_prefix $install_prefix
 # build the project
 ./build.sh
 # update the env vars if changed
-source _env_vars_file.sh
+source env/scripts/private/_env_vars_file.sh
 
 # activate python's virtual env
 source ./activate.sh --quiet
