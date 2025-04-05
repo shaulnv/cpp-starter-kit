a12b635e716eab5bea56fbd093f914d9b485ffe7
Shaul Fridman <shaulf@nvidia.com>
2025-04-05 03:30:27 +0300

    (~) move 'cmake' folder to env, move private scripts to their own folder

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the build flags & activate the environment
-source ./env/_build_env_vars.sh "$@"
+source ./env/scripts/private/_build_env_vars.sh "$@"
 
 # What we're building
 echo Profile: $profile
