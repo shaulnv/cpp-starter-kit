72a855ffd4f029308e2b248c8a7cfe4e5af7171a
Shaul Fridman <shaulf@nvidia.com>
2025-04-09 21:00:22 +0300

    (+) support devcontainers development env
    Add a Dev Container dev-env supprt.
    This is a Docerfile + configuration JSON file.
    In VSCode: Ctrl+Shift+P: Dev Containers: Open Workspace in Container
    In GitHub: Repo/Green Button "Code"/Codespaces

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -Eex
+set -o pipefail
+
+root_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+red="\033[0;31m"
+green="\033[0;32m"
+nc='\033[0m' # No Color
+
+install-github-cli() {
+  sudo curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+  sudo apt -y update
+  sudo apt -y install gh
+}
+
+# Set noninteractive mode for apt
+DEBIAN_FRONTEND=noninteractive
+
+# make sure we can install OS packages
+# either sudo or root
+if ! command -v sudo &>/dev/null; then
+  if [ "$EUID" -ne 0 ]; then
+    echo -e "${red}sudo is not installed, and you are not root. run once: apt update -y && apt install -y sudo${nc}"
+    exit 1
+  fi
+  echo -e "${green}sudo is not installed, installing...${nc}"
+  apt update -y
+  apt install -y --no-install-recommends sudo
+fi
+
+# install OS packages
+sudo apt update -y
+sudo apt install -y --no-install-recommends \
+  sudo curl wget gnupg tig vim \
+  python3 python-is-python3 python3-pip python3-dev python3-venv python3-setuptools \
+  git build-essential clang gdb \
+  nodejs
+
+install-github-cli
+
+# Clean up OS packages
+sudo apt clean -y
+sudo rm -rf /var/lib/apt/lists/*
