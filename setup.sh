dc22f7696f6cde909d87e63e8684cf3c788ff896
Shaul Fridman <shaulf@nvidia.com>
2025-04-17 13:33:56 +0000

    (~) cleaning and re-orginize setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
+# to ease debugging of installation issues
+set -x
+
+# BASH error handling:
+#   exit on command failure
 set -Eex
 set -o pipefail
+#   keep track of the last executed command
+trap 'LAST_COMMAND=$CURRENT_COMMAND; CURRENT_COMMAND=$BASH_COMMAND' DEBUG
+#   on error: print the failed command
+trap 'ERROR_CODE=$?; FAILED_COMMAND=$LAST_COMMAND; tput setaf 1; echo "ERROR: command \"$FAILED_COMMAND\" failed with exit code $ERROR_CODE"; tput sgr0;' ERR INT TERM
 
+# common variables
 root_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 red="\033[0;31m"
 green="\033[0;32m"
 nc='\033[0m' # No Color
+
+package-manager-setup() {
+  # Set noninteractive mode for apt
+  DEBIAN_FRONTEND=noninteractive
+  # make sure we can install OS packages
+  # either sudo or root
+  if ! command -v sudo &>/dev/null; then
+    if [ "$EUID" -ne 0 ]; then
+      echo -e "${red}sudo is not installed, and you are not root. run once: apt -y update && apt -y install sudo${nc}"
+      exit 1
+    fi
+    echo -e "${green}sudo is not installed, installing...${nc}"
+    apt -y update
+    apt -y install --no-install-recommends sudo
+  fi
+  sudo apt -y update
+  # packages for installing other packages from the internet
+  sudo apt -y install --no-install-recommends curl wget gnupg
+}
+
+package-manager-cleanup() {
+  # Clean up OS packages
+  sudo apt -y clean
+  sudo rm -rf /var/lib/apt/lists/*
+}
 
 install-github-cli() {
   sudo curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -16,39 +51,41 @@ install-github-cli() {
   sudo apt -y install gh
 }
 
-# Set noninteractive mode for apt
-DEBIAN_FRONTEND=noninteractive
+install-dev-tools() {
+  # Tools for development, not a pre-requisite for building the project
+  # clang packages are needed for clang-tidy build
+  sudo apt -y install --no-install-recommends \
+    git tig vim tree \
+    python3 python-is-python3 python3-pip python3-dev python3-venv python3-setuptools \
+    clang libc++-dev libc++abi-dev llvm \
+    gdb \
+    nodejs
+  install-github-cli
+}
 
-# make sure we can install OS packages
-# either sudo or root
-if ! command -v sudo &>/dev/null; then
-  if [ "$EUID" -ne 0 ]; then
-    echo -e "${red}sudo is not installed, and you are not root. run once: apt update -y && apt install -y sudo${nc}"
-    exit 1
-  fi
-  echo -e "${green}sudo is not installed, installing...${nc}"
-  apt update -y
-  apt install -y --no-install-recommends sudo
-fi
+install-build-dependencies() {
+  # build dependencies
+  # curl is needed to install uv
+  sudo apt -y install --no-install-recommends \
+    curl build-essential
+  # install uv (the Python's package manager used by the project)
+  source $root_dir/env/scripts/_install_uv.sh
+}
 
-# install OS packages
-sudo apt update -y
-sudo apt install -y --no-install-recommends \
-  sudo curl wget gnupg tig vim \
-  python3 python-is-python3 python3-pip python3-dev python3-venv python3-setuptools \
-  git build-essential clang libc++-dev libc++abi-dev llvm gdb \
-  nodejs
+install-vscode-extensions-dependencies() {
+  # .NET is needed by VSCode's CMake language extension
+  curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+  chmod +x ./dotnet-install.sh
+  sudo ./dotnet-install.sh --version latest --runtime aspnetcore
+}
 
-install-github-cli
-# install uv (the Python's package manager used by the project)
-source $root_dir/env/scripts/_install_uv.sh
-
-# Clean up OS packages
-sudo apt clean -y
-sudo rm -rf /var/lib/apt/lists/*
-
-# install uv (a Python's package manager)
-curl -LsSf https://astral.sh/uv/install.sh | sh
+# setup the environment
+package-manager-setup
+install-dev-tools
+install-build-dependencies
+install-vscode-extensions-dependencies
+package-manager-cleanup
 
 # mark setup as done
 touch $root_dir/.env
+echo -e "${green}setup done${nc}"
