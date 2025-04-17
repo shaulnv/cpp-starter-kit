bdb5f717ac672e9ced40eb7f5f08c077fa2ecb23
Shaul Fridman <shaulf@nvidia.com>
2025-04-17 14:52:43 +0300

    (*) change: building to a new profile doesn't do a clean-build
    Now you can build regulary, and change to build with clang-tidy and the
    build folder is preserved :-)

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo Build: $build_type
 echo Build Folder: $build_folder
 
 if [[ "$clean" == "true" ]]; then
-  rm -rf $build_folder
+  rm -rf $build_folder/*
 fi
 if [ ! -f "$build_folder/CMakeCache.txt" ]; then
   conan build . --build=missing -pr:b ./env/profiles/native.profile -pr:h ./env/profiles/$profile.profile -s:a build_type=$build_type
